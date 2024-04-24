91a0357e8897a2abf9834ee1f66ec7799ddbd6b8
arnavnagpurkar <arnavn.dev@gmail.com>
2024-04-24 19:19:48 +0530

    Basic Sidebar

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import Sidebar from '@/components/Sidebar'
 import React, { ReactNode } from 'react'
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
@@ -8,7 +9,7 @@ const HomeLayout = ({ children }: { children: ReactNode }) => {
 
       <div className="flex">
 
-        Sidebar
+        <Sidebar />
 
         <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
 
