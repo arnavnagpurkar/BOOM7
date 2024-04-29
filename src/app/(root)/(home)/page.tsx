39aa99f8422e9c09cb7bc98a7dc6f11f4a15b9e3
arnavnagpurkar <arnavn.dev@gmail.com>
2024-04-29 21:41:04 +0530

    Current date and time

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
+  const formattedTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }).toUpperCase();
+  const formattedDate = () => {
+    const d = new Date();
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return `${days[d.getDay()]}, ${d.getDate()} ${months[d.getMonth()]}, ${d.getFullYear()}`;
+  };
+
+
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
-      <h1 className='text-3xl font-bold'>
-        Home
-      </h1>
+      <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
+        <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
+          <h2 className='glassmorphism max-w-[220px] rounded py-2 text-center text-base font-normal'>
+            No Upcoming Meetings
+          </h2>
+          <div className='flex flex-col gap-2'>
+            <h1 className='text-4xl font-extrabold lg:text-7xl'>
+              {formattedTime}
+            </h1>
+            <p className='text-lg font-medium text-sky-1 lg:text-2xl'>
+              {formattedDate()}
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
