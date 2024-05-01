51a3f479c5237ec98be0a53eda9b3d9cfa951f63
arnavnagpurkar <arnavn.dev@gmail.com>
2024-05-01 12:56:31 +0530

    Reusable Modal and minor things

---
+++ src/components/MeetingModal.tsx
@@ -0,0 +1,53 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { cn } from "@/lib/utils"
+import Image from "next/image"
+import { Button } from "./ui/button"
+
+
+const MeetingModal = (props: any) => {
+  return (
+    <Dialog open={props.isOpen} onOpenChange={props.onClose}>
+      <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
+        <div className="flex flex-col gap-6">
+          {props.image && (
+            <div className="flex justify-center">
+              <Image
+                src={props.image}
+                alt="image"
+                width={72}
+                height={72}
+              />
+            </div>
+          )}
+          <h1 className={cn('text-3xl font-bold leading-[42px]', props.className)}>
+            {props.title}
+          </h1>
+          {props.children}
+          <Button
+            className="bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0 rounded-[8px]"
+            onClick={props.clickFunction}
+          >
+            {props.buttonIcon && (
+              <Image
+                src={props.buttonIcon}
+                alt="button icon"
+                width={13}
+                height={13}
+              />
+            )} &nbsp;
+            {props.buttonText || 'Schedule Meeting'}
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default MeetingModal
--- src/components/MeetingTypeList.tsx
+++ src/components/MeetingTypeList.tsx
@@ -2,9 +2,14 @@
 import { useState } from 'react'
 import HomeCard from './HomeCard'
 import Link from 'next/link'
+import MeetingModal from './MeetingModal'
 
 const MeetingTypeList = () => {
   const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
+
+  const createMeeting = () => {
+    
+  }
 
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
@@ -44,6 +49,15 @@ const MeetingTypeList = () => {
           description="Meeting Recordings"
         />
       </Link>
+
+      <MeetingModal 
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        clickFunction={createMeeting}
+      />
     </section>
   )
 }
