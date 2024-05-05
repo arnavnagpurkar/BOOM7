b621af795c362f6447f6373c4df5eb90e614720c
arnavnagpurkar <arnavn.dev@gmail.com>
2024-05-05 17:52:00 +0530

    functions created, starting with tsx

--- src/components/CallList.tsx
+++ src/components/CallList.tsx
@@ -1,9 +1,41 @@
 "use client"
-import React from 'react'
+import { useState } from 'react'
 import useGetCalls from '@/hooks/useGetCalls';
+import { useRouter } from 'next/navigation';
+import { CallRecording } from '@stream-io/video-react-sdk';
 
 const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
-  const { endedCalls, upcomingCalls, callRecordings, isLoading } = useGetCalls()
+  const { endedCalls, upcomingCalls, callRecordings, isLoading } = useGetCalls();
+  const [recordings, setRecordings] = useState<CallRecording[]>([]);
+  const router = useRouter();
+
+  const getCalls = () => {
+    switch (type) {
+      case 'ended':
+        return endedCalls;
+      case 'recordings':
+        return recordings;
+      case 'upcoming':
+        return upcomingCalls;
+
+      default:
+        return [];
+    }
+  }
+
+  const getNoCallsMessage = () => {
+    switch (type) {
+      case 'ended':
+        return 'No Previous Calls';
+      case 'recordings':
+        return 'No Recordings';
+      case 'upcoming':
+        return 'No Upcoming Calls';
+
+      default:
+        return '';
+    }
+  }
 
   return (
     <div>
