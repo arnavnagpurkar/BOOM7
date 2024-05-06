128c75377c070785745aec43474dbfbf30e56b3a
arnavnagpurkar <arnavn.dev@gmail.com>
2024-05-06 13:45:26 +0530

    Personal Room Invite link change

--- src/app/(root)/(home)/personal-room/page.tsx
+++ src/app/(root)/(home)/personal-room/page.tsx
@@ -28,7 +28,7 @@ const PersonalRoom = () => {
 
   const { call } = useGetCallById(meetingId!);
 
-  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}?personal=true`;
+  const meetingLink = `https://${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}?personal=true`;
 
   const startRoom = async () => {
     if (!client || !user) return;
