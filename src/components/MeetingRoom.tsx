5b71618abff21bcdc2a4c569dec4b6eb1e499e62
arnavnagpurkar <arnavn.dev@gmail.com>
2024-05-06 13:02:26 +0530

    Meeting leave bug fixed

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { LayoutList, Users } from 'lucide-react';
 import { Button } from './ui/button';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import EndCallButton from './EndCallButton';
 import Loader from './Loader';
 
@@ -12,6 +12,8 @@ import Loader from './Loader';
 type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right'
 
 const MeetingRoom = () => {
+  const router = useRouter();
+
   const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
 
   const [showParticipants, setShowParticipants] = useState(false);
@@ -49,7 +51,11 @@ const MeetingRoom = () => {
       </div>
 
       <div className='fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap'>
-        <CallControls />
+        <CallControls
+          onLeave={() => {
+            router.push('/')
+          }}
+        />
 
         <DropdownMenu>
 
