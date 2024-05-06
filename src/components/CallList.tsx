f969ea0c7dc64ed5ee2e1c31cb3a81c900ada639
arnavnagpurkar <arnavn.dev@gmail.com>
2024-05-06 12:36:50 +0530

    Displaying errors

--- src/components/CallList.tsx
+++ src/components/CallList.tsx
@@ -1,16 +1,18 @@
 'use client';
 import { Call, CallRecording } from '@stream-io/video-react-sdk';
 import Loader from './Loader';
-import useGetCalls  from '@/hooks/useGetCalls';
+import useGetCalls from '@/hooks/useGetCalls';
 import MeetingCard from './MeetingCard';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { useToast } from './ui/use-toast';
 
 const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
   const router = useRouter();
   const { endedCalls, upcomingCalls, callRecordings, isLoading } =
     useGetCalls();
   const [recordings, setRecordings] = useState<CallRecording[]>([]);
+  const { toast } = useToast();
 
   const getCalls = () => {
     switch (type) {
@@ -40,15 +42,20 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
 
   useEffect(() => {
     const fetchRecordings = async () => {
-      const callData = await Promise.all(
-        callRecordings?.map((meeting) => meeting.queryRecordings()) ?? [],
-      );
+      try {
+        const callData = await Promise.all(
+          callRecordings?.map((meeting) => meeting.queryRecordings()) ?? [],
+        );
 
-      const recordings = callData
-        .filter((call) => call.recordings.length > 0)
-        .flatMap((call) => call.recordings);
+        const recordings = callData
+          .filter((call) => call.recordings.length > 0)
+          .flatMap((call) => call.recordings);
 
-      setRecordings(recordings);
+        setRecordings(recordings);
+
+      } catch (error) {
+        toast({ title: 'Error while fetching', description: 'Try again later' })
+      }
     };
 
     if (type === 'recordings') {
